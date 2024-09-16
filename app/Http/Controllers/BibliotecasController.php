2569f22c66ad2f6975e3d2efb522f94ad7feceef
Francisco Filipe <franciscofilipeco2007@gmail.com>
2024-09-16 13:00:19 -0300

    New: BibliotecasController and Emprestimos controller new routes

--- app/Http/Controllers/BibliotecasController.php
+++ app/Http/Controllers/BibliotecasController.php
@@ -3,9 +3,54 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bibliotecas;
 use Illuminate\Http\Request;
 
 class BibliotecasController extends Controller
 {
-    //
+    public function index()
+    {
+        return view();
+    }
+
+    public function store(Request $request)
+    {
+        $biblioteca = Bibliotecas::create([
+            'nome' => $request->nome,
+            'logradouro' => $request->logradouro,
+            'cidade' => $request->cidade,
+            'estado' => $request->estado,
+            'cep' => $request->cep,
+            'escola' => $request->escola
+        ]);
+
+        return response()->json($biblioteca);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $biblioteca = Bibliotecas::findOrFail($id)->update([
+            'nome' => $request->nome,
+            'logradouro' => $request->logradouro,
+            'cidade' => $request->cidade,
+            'estado' => $request->estado,
+            'cep' => $request->cep,
+            'escola' => $request->escola
+        ]);
+    }
+
+    public function listAll()
+    {
+        $Bibliotecas = Bibliotecas::get();
+    }
+
+    public function list($id)
+    {
+        Bibliotecas::findOrFail($id)->first();
+    }
+
+    public function destroy($id)
+    {
+        Bibliotecas::findOrFail($id)->delete();
+    }
 }
--- app/Http/Controllers/EmprestimosController.php
+++ app/Http/Controllers/EmprestimosController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Emprestimos;
 use Illuminate\Http\Request;
 
 class EmprestimosController extends Controller
 {
-    //
+    public function index()
+    {
+        return view();
+    }
+
+    public function store(Request $request)
+    {
+        $emprestimo = Emprestimos::create([
+            'livro_id' => $request->livro_id,
+            'user_id' => $request->user_id,
+            'biblioteca_id' => $request->biblioteca_id,
+            'inicio' => now(),
+            'final' => $request->final
+        ]);
+
+        return response()->json($emprestimo);
+    }
+
+    public function update(Request $request, $id)
+    {
+        Emprestimos::findOrFail($id)->update([
+            'final' => $request->final
+        ]);
+    }
+
+    public function createQrCode() {}
+
+    public function joinQrCode() {}
 }
