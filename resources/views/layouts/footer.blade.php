fc9c438ee9087b5fbb2430c38d8318bf829235b2
Francisco Filipe <franciscofilipeco2007@gmail.com>
2024-09-18 07:07:41 -0300

    Edit: Added media kit on site index

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="w-100 d-flex justify-content-center p-2 bg-dark">
+    <div>
+        <h5 class="text-white">© 2024 Be Reader</h1>
+    </div>
+</footer>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,10 +5,13 @@
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse justify-content-start" id="navbarNav">
+            <a href="/"><img src="{{ url('/images/logo_withoutbg.png') }}" width="40px"></a>
+        </div>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Página inicial</a>
+                    <a class="nav-link" href="/">Página inicial</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Livros</a>
@@ -21,7 +24,7 @@
                         <a class="nav-link" href="#">Minha conta</a>
                     @else
                         <div class="btn-group" role="group" aria-label="Basic example">
-                            <a href="/login" class="btn btn-light">Log-in</a>
+                            <a href="/login" class="btn btn-light">Entrar</a>
                             <a href="/register" class="btn btn-light">Cadastre-se</a>
                         </div>
                     @endif
