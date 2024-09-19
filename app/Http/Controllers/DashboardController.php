8f520bccd98c1170298922b90a6285111111d5d8
Francisco Filipe <franciscofilipeco2007@gmail.com>
2024-09-18 22:14:52 -0300

    Edit: Updated Dashboard controller and dashboard blade

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Avaliacoes;
+use App\Models\Bibliotecas;
+use App\Models\Emprestimos;
+use App\Models\Livros;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $livros = Livros::all();
+        $emprestimos = Emprestimos::where('user_id', Auth::user()->id)->get();
+        $bibliotecas = Bibliotecas::all();
+        $resenhas = Avaliacoes::where('user_id', Auth::user()->id)->get();
+        return view('dashboard.usuario', ["livros" => $livros, "emprestimos" => $emprestimos, "bibliotecas" => $bibliotecas, "resenhas" => $resenhas]);
+    }
 }
