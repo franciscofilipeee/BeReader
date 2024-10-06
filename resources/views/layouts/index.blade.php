c2e1340a46d248cf8c8c183c9946fa3321f2dbd5
Francisco Filipe <franciscofilipeco2007@gmail.com>
2024-10-06 18:34:06 -0300

    New: Added jquery

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,5 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Be Reader - Uma nova forma de ler</title>
     <link rel="shortcut icon" href="{{ url('/images/logo_withoutbg.png') }}" type="image/x-icon">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
